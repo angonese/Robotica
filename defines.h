e882998979cd0256d9040246d1ed2d90b2d38c3d
johnfercher <johnathanfercher22@gmail.com>
2013-06-20 23:56:18 -0300

    Create defines.h
    definindo os headers de cada arquivo

---
+++ defines.h
@@ -0,0 +1,9 @@
+//crie um arquivo chamado cores.cpp e insira esse código.
+#ifndef CORES_H
+#define CORES_H
+#endif
+
+//crie um arquivo chamado processamento.cpp e insira esse código
+#ifndef PROCESSAMENTO_H
+#define PROCESSAMENTO_H
+#endif
